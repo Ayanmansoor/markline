f97a36c165cf7c8a3819ba9deda2e38cec6ec84d
Mohammad Ayan <ayanmansoor0919@gmail.com>
2025-09-07 20:49:55 +0530

    ayan

--- components/Common/CartSheet.tsx
+++ components/Common/CartSheet.tsx
@@ -41,7 +41,7 @@ function CartSheet({ children }: {
     const { executeRecaptcha } = useGoogleReCaptcha()
     const [UserAddress, setUserAddress] = useState<AddressProps | undefined>()
 
-
+    const [side, setSide] = useState<"right" | "bottom">("right")
 
     useEffect(() => {
         async function getSupabaseUser() {
@@ -169,7 +169,6 @@ function CartSheet({ children }: {
 
         }
     }
-
     async function Razorpayment(orderedData: OrderProps[], UserAddress: AddressProps | undefined) {
 
 
@@ -226,22 +225,35 @@ function CartSheet({ children }: {
         }
     }
 
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth <= 600) {
+                setSide("bottom")
+            } else {
+                setSide("right")
+            }
+        }
+        handleResize() // run on mount
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+    }, [])
+
     return (
         <Sheet >
             <SheetTrigger className='w-fit h-fit relative '>{children}</SheetTrigger>
-            <SheetContent className="py-5  px-2 max-w-[550px] md:min-w-[550px] h-full  overflow-y-auto" id="style-3">
+            <SheetContent side={side} className="py-5 h-[600px]   px-2 max-w-[550px] md:min-w-[550px] sm:h-full  overflow-y-auto" id="style-3">
                 <SheetHeader>
                     <SheetTitle className="font-medium text-2xl border-b pb-2 text-start">Carts</SheetTitle>
 
-                    <section className='w-full relative h-auto grid grid-cols-1 sm:grid-cols-2 gap-1'>
+                    <section className='w-full relative max-h-full  overflow-y-auto grid grid-cols-2 md:grid-cols-1 gap-1  '>
                         {
                             cart.length > 0 ? cart.map((item, index) => (
                                 <div key={index} className='w-full border p-2 rounded-lg relative h-auto flex flex-col md:grid md:grid-cols-[100px_2fr_auto_auto] items-start md:items-center justify-between gap-1'>
 
                                     <Swiper pagination={{ dynamicBullets: true }} modules={[Pagination]} className="mySwiper w-full relative h-full">
                                         {item?.variant?.image_url.map((image, index: number) => (
-                                            <SwiperSlide key={index} className='w-full  h-[100px] md:h-full relative'>
-                                                <img src={image.image_url} alt={image?.name} className='  w-full md:w-full h-[100x] md:max-h-full  object-contain rounded-md border' loading='lazy' />
+                                            <SwiperSlide key={index} className='w-full  max-h-[100px] md:max-h-full relative'>
+                                                <img src={image.image_url} alt={image?.name} className='  aspect-square w-full md:w-full h-full md:max-h-full  object-contain rounded-md border' loading='lazy' />
                                             </SwiperSlide>
                                         ))}
                                     </Swiper>
@@ -283,7 +295,7 @@ function CartSheet({ children }: {
                                     </div>
                                 </div>
                             )) : (
-                                <div className='text-p20 font-medium py-10 px-5 flex items-center justify-center flex-col text-primary'>
+                                <div className='text-p20 col-span-2 font-medium py-10 px-5 flex items-center justify-center flex-col text-primary'>
                                     <ShoppingBag className="h-10 w-10 mx-auto text-gray-400 mb-8" />
                                     Your cart is empty
                                 </div>
@@ -291,7 +303,7 @@ function CartSheet({ children }: {
                         }
                     </section>
 
-                    <SheetFooter className='absolute bottom-5 w-full left-0 bg-white '>
+                    <section className='absolute bottom-5 w-full left-0 bg-white '>
                         {
                             totalPrice > 0 &&
                             <section className='w-full relative z-30 p-3 h-fit  flex flex-col gap-2 border bg-white  '>
@@ -329,13 +341,54 @@ function CartSheet({ children }: {
 
                             </section>
                         }
-                    </SheetFooter>
+                    </section>
 
                 </SheetHeader>
             </SheetContent>
         </Sheet >
-
     )
 }
 
 export default CartSheet
+
+
+
+//  <section className='absolute bottom-5 w-full left-0 bg-white '>
+//                     {
+//                         totalPrice > 0 &&
+//                         <section className='w-full relative z-30 p-3 h-fit  flex flex-col gap-2 border bg-white  '>
+//                             <h2 className='text-base font-semibold text-primary text-start'>After Discount</h2>
+//                             <span className='w-full relative h-auto bg-gray-100 flex items-center  justify-center px-3 py-1'>
+//                                 <p className='text-base w-full font-medium text-foreground '>Total </p>
+//                                 <p className='text-lg font-medium text-primary w-full'>₹ {beforeDiscount}</p>
+//                             </span>
+//                             {
+//                                 totalDiscount < 0 &&
+//                                 <span className='w-full relative h-auto  flex items-center  justify-center px-3 py-1 '>
+//                                     <p className=' text-sm w-full font-medium text-foreground '>Discount</p>
+//                                     <p className='text-sm text-red-400 line-through font-medium text-primary w-full '>₹ {Math.floor(totalDiscount)}</p>
+//                                 </span>
+//                             }
+
+//                             <div className='w-fulll border-t pt-2  relative h-auto px-3 flex items-center justify-between'>
+//                                 <p className='text-sm sm:text-base md:text-lg font-medium w-full text-primary'>Price To Pay</p>
+//                                 <p className='text-lg font-medium text-primary w-full'>₹ {Math.floor(totalPrice)}</p>
+//                             </div>
+//                             <div className='w-full relative h-auto flex items-start justify-end'>
+//                                 {
+//                                     UserAddress?.id &&
+//                                     <button className='w-fit relative h-auto flex items-center  text-sm md:text-base border border-primary px-4 md:px-5 py-2 text-nowrap bg-primary  text-white  font-medium text-primary' disabled={isOrderSub} onClick={OrdersBeforePayment}  >{isOrderSub ? "Just a second..." : "Buy Now"}</button>
+
+//                                 }
+//                                 {
+//                                     !UserAddress?.id && !UserAddress?.id &&
+//                                     <CartSheetOderDailog closeSheet={() => { }}>
+//                                         <button className='w-fit relative h-auto flex items-center  text-sm md:text-base border border-primary px-4 md:px-5 py-2 text-nowrap bg-primary  text-white  font-medium text-primary'>Place Order</button>
+//                                     </CartSheetOderDailog>
+//                                 }
+
+//                             </div>
+
+//                         </section>
+//                     }
+//                 </section>
