e9826097780b915e8dfe3710d3df203e69a504da
shaikhAyan1 <ayanmansoor0919@gmail.com>
2026-02-20 17:21:55 +0530

    new chnages

--- app/(main)/collections/[group]/[collection]/page.tsx
+++ app/(main)/collections/[group]/[collection]/page.tsx
@@ -1,63 +1,124 @@
 import CategoryL2page from '@/components/Collection/CollectionPage'
-import { getAllCollectionsBaseOnGender } from '@/Supabase/SupabaseApi';
+import { getAllCollectionsBaseOnGender, getaudience } from '@/Supabase/SupabaseApi';
+import { AudienceProps } from '@/types/interfaces';
 import React from 'react'
+// export async function generateMetadata({ params }) {
+
+//   console.log("Current Collection:", params); // Debug log to check the collection data
+
+
+//   // const collections: any[] | Error | null = await getAllCollectionsBaseOnGender(params.group);
+
+//   // if (collections instanceof Error || !collections) {
+//   //   return {
+//   //     title: "Collections | Markline",
+//   //     description: "Explore our latest footwear collections for men, women, and kids at Markline.",
+//   //   };
+//   // }
+
+//   // const currentCollection = collections.find((c: any) => c.slug === params.group);
+
+//   // if (!currentCollection) {
+//   //   return {
+//   //     title: "Collections | Markline",
+//   //     description: "Explore our latest footwear collections for men, women, and kids at Markline.",
+//   //   };
+//   // }
+
+
+//   // console.log("Current Collection:", currentCollection); // Debug log to check the collection data
+//   // const collectionName =
+//   //   currentCollection?.seoTitle?.trim() ||
+//   //   currentCollection?.name ||
+//   //   "Collection";
+
+//   // const description =
+//   //   currentCollection?.seoDescription?.trim() ||
+//   //   `Shop ${currentCollection?.name || "our footwear"} at Markline – stylish options for men, women & kids.`;
+
+//   // return {
+//   //   title: `${collectionName} | Markline`,
+//   //   description,
+//   //   keywords: [
+//   //     "Markline",
+//   //     currentCollection?.name,
+//   //     "Men's Footwear",
+//   //     "Women's Footwear",
+//   //     "Kids Shoes",
+//   //     "Sandals",
+//   //     "Heels",
+//   //     "Casual Shoes",
+//   //     "Mules",
+//   //   ],
+//   //   openGraph: {
+//   //     title: `${collectionName} | Markline`,
+//   //     description,
+//   //     url: `https://shopmarkline.in/collection/${params.group}`,
+//   //     images: currentCollection?.image_urls?.map((img: string) => {
+//   //       try {
+//   //         const parsed = JSON.parse(img);
+//   //         return {
+//   //           url: parsed.image_url,
+//   //           alt: currentCollection?.name,
+//   //         };
+//   //       } catch {
+//   //         return null;
+//   //       }
+//   //     }).filter(Boolean),
+//   //   },
+//   //   robots: {
+//   //     index: true,
+//   //     follow: true,
+//   //     "max-image-preview": "large",
+//   //     "max-snippet": -1,
+//   //     "max-video-preview": -1,
+//   //   },
+//   //   twitter: {
+//   //     card: "summary_large_image",
+//   //     title: `${collectionName} | Markline`,
+//   //     description,
+//   //   },
+//   //   alternates: {
+//   //     canonical: `https://shopmarkline.in/collection/${params?.group}`,
+//   //   },
+//   // };
+// }
+
 export async function generateMetadata({ params }) {
-  const collections: any[] | Error | null = await getAllCollectionsBaseOnGender(params.group);
 
-  if (collections instanceof Error || !collections) {
-    return {
-      title: "Collections | Markline",
-      description: "Explore our latest footwear collections for men, women, and kids at Markline.",
-    };
-  }
 
-  const currentCollection = collections.find((c: any) => c.slug === params.group);
+  const collectionsSlug = `${params.collection}`
+  const groupSlug = `${params.group}`
 
-  if (!currentCollection) {
-    return {
-      title: "Collections | Markline",
-      description: "Explore our latest footwear collections for men, women, and kids at Markline.",
-    };
-  }
+  const audience: AudienceProps | null = await getaudience(`${params.group}`.toUpperCase());
 
-  const collectionName =
-    currentCollection?.seoTitle?.trim() ||
-    currentCollection?.name ||
-    "Collection";
 
+  const audienceName = audience?.seo_title || "Audience";
   const description =
-    currentCollection?.seoDescription?.trim() ||
-    `Shop ${currentCollection?.name || "our footwear"} at Markline – stylish options for men, women & kids.`;
-
+    audience?.seo_discription ||
+    `Explore Markline footwear collections – ${collectionsSlug} shoes for men, women & kids.`;
   return {
-    title: `${collectionName} | Markline`,
+    title: `${audienceName} | Markline`,
     description,
     keywords: [
       "Markline",
-      currentCollection?.name,
+      audienceName,
       "Men's Footwear",
       "Women's Footwear",
       "Kids Shoes",
       "Sandals",
       "Heels",
       "Casual Shoes",
-      "Mules",
     ],
     openGraph: {
-      title: `${collectionName} | Markline`,
+      title: `${audienceName} | Markline`,
       description,
-      url: `https://shopmarkline.in/collection/${params.group}`,
-      images: currentCollection?.image_urls?.map((img: string) => {
-        try {
-          const parsed = JSON.parse(img);
-          return {
-            url: parsed.image_url,
-            alt: currentCollection?.name,
-          };
-        } catch {
-          return null;
-        }
-      }).filter(Boolean),
+      url: `https://shopmarkline.in/collections/${groupSlug}/${collectionsSlug}`,
+      images: [
+        {
+          alt: audienceName,
+        },
+      ],
     },
     robots: {
       index: true,
@@ -68,15 +129,14 @@ export async function generateMetadata({ params }) {
     },
     twitter: {
       card: "summary_large_image",
-      title: `${collectionName} | Markline`,
+      title: `${audienceName} | Markline`,
       description,
     },
     alternates: {
-      canonical: `https://shopmarkline.in/collection/${params?.group}`,
+      canonical: `https://shopmarkline.in/collections/${groupSlug}/${collectionsSlug}`,
     },
   };
 }
-
 
 function page() {
   return (
